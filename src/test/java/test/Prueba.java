551febf44191552051f4c092dc0fd13880bf66be
sebamaximus <acuna.antinao.sebastian@gmail.com>
2021-04-12 01:47:22 -0400

    Cambios en Prueba.java, se eliminan dos lineas de codigo

--- src/test/java/test/Prueba.java
+++ src/test/java/test/Prueba.java
@@ -11,9 +11,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import common.CrearRut;
-import evidence.CrearLogyDocumento;
-
 public class Prueba {
 	private WebDriver driver;
 	@BeforeMethod
